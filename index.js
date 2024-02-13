1fc4a7af1bf343bb8bc50a1f7737eb38af2f1cd4
unknown <amr.magdy.ahmed92@gmail.com>
2024-02-13 12:30:33 +0200

    Added unitselection funtionality

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function genderSelection(element) {
     formSelections["gender"] = element.innerText
     document.getElementById("gender").innerHTML = element.innerHTML
 }
+
+function unitSelection(element) {
+    formSelections["unit"] = element.innerText
+    document.getElementById("unit").innerHTML = element.innerHTML
+}
