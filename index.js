014ee0ecdb9daef0f445872c2478a863171b9d18
unknown <amr.magdy.ahmed92@gmail.com>
2024-02-13 13:59:10 +0200

    added changing the form labels to the unitSelection function

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 let units
+let heightText = document.getElementById("height-label").innerText
+let weightText = document.getElementById("weight-label").innerText
 
 function unitSelection(element) {
+    if (element.innerText.toLowerCase() === "metric") {
+        document.getElementById("height-label").innerText = `${heightText} (cm)`
+        document.getElementById("weight-label").innerText = `${weightText} (kg)`
+    } else if (element.innerText.toLowerCase() == "imperial") {
+        document.getElementById("height-label").innerText = `${heightText} (in)`
+        document.getElementById("weight-label").innerText = `${weightText} (lb)`
+    }
     units = element.innerText.toLowerCase()
     document.getElementById("unit").innerHTML = element.innerHTML
 }
