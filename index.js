6c098bd7776cfe7145005d44546ac6ea0115077d
unknown <amr.magdy.ahmed92@gmail.com>
2024-02-13 12:14:06 +0200

    Made the gender drop down show the user's selection

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ let formSelections = {}
 
 function genderSelection(element) {
     formSelections["gender"] = element.innerText
+    document.getElementById("gender").innerHTML = element.innerHTML
 }
